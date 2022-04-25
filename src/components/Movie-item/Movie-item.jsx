eadfc016894b1d25eedd176e887a94c210b35722
gre1pk <raik1996@gmail.com>
2022-04-25 20:20:31 +0300

    feat: add reiting

--- src/components/Movie-item/Movie-item.jsx
+++ src/components/Movie-item/Movie-item.jsx
@@ -1,25 +1,57 @@
-import { Card, Typography, Button } from 'antd'
+import { Card, Typography, Tag, Rate } from 'antd'
 import './Movie-item.css'
 
-function MovieItem({ title, releaseDate, posterPath, overview }) {
+import { GenresConsumer } from '../Genres-context'
+
+function MovieItem({ title, releaseDate, posterPath, overview, voteAverage, movieGenre }) {
   const { Text, Title } = Typography
+
+  const colorReiting = (reit) => {
+    if (reit > 7) {
+      return 'max'
+    }
+    if (reit > 5) {
+      return 'norm'
+    }
+
+    if (reit > 3) {
+      return 'poor'
+    }
+    return 'low'
+  }
+
+  const generis = (genresId, movieGenreList) => {
+    const element = movieGenreList.map((id) => {
+      const re = genresId.filter((el) => el.id === id)
+      return (
+        <Tag key={id} size="small">
+          {re[0].name}
+        </Tag>
+      )
+    })
+    return element
+  }
+
   return (
     <Card className="card">
       <div className="card__img">
         <img alt={`poster ${title}`} src={`https://image.tmdb.org/t/p/w500/${posterPath} `} />
       </div>
-      <div className="card_content">
-        <Title level={4} className="card__movie-title">
-          {title}
-        </Title>
+      <div className="card__content">
+        <div className="card__header">
+          <Title level={4} className="card__movie-title">
+            {title}
+          </Title>
+          <div className={`card__reiting ${colorReiting(voteAverage)}`}>{voteAverage}</div>
+        </div>
         <Text type="secondary" className="card__release-date">
           {releaseDate}
         </Text>
-        <Button size="small" style={{ marginRight: 8 }}>
-          Action
-        </Button>
-        <Button size="small">Dramma</Button>
+        <div className="div">
+          <GenresConsumer>{(genresId) => generis(genresId, movieGenre)}</GenresConsumer>
+        </div>
         <Text className="card__overview">{overview}</Text>
+        <Rate className="card__rate" value={2} />
       </div>
     </Card>
   )
