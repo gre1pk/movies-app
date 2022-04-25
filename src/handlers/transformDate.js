6034a6b1c546c8c3695985545c7533ffec16e135
gre1pk <raik1996@gmail.com>
2022-04-25 20:21:14 +0300

    feat: add context

--- src/handlers/transformDate.js
+++ src/handlers/transformDate.js
@@ -1,10 +1,12 @@
-function trasformDate(re) {
+function trasformDate(movie) {
   return {
-    id: re.id,
-    originalTitle: re.original_title,
-    releaseDate: re.release_date,
-    posterPath: re.poster_path,
-    overview: re.overview,
+    id: movie.id,
+    originalTitle: movie.original_title,
+    releaseDate: movie.release_date,
+    posterPath: movie.poster_path,
+    overview: movie.overview,
+    voteAverage: movie.vote_average,
+    movieGenre: movie.genre_ids,
   }
 }
 
